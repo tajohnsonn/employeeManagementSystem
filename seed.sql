355853c3d2de4b1152cc4e1b5736fa208682cad1
sunshine <tajohnsonn@gmail.com>
2020-09-22 19:43:19 -0700

    adding tables and dummy test info

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("Legal");
+INSERT INTO department (name) VALUES ("Engineering");
+
+
+  

2724437522389f174fae92564a68fc6270110c07
sunshine <tajohnsonn@gmail.com>
2020-09-29 18:43:16 -0700

    finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cTable = require("console.table");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const util = require("util");
@@ -28,104 +29,120 @@ connection.connect(async err => {
 
   //.then.catch
   //async/await
-  try {
-    const todoAnswer = await inquirer.prompt({
-      type: "list",
-      name: "todo",
-      message: "What do you want to do?",
-      choices: [
-        "Add a Department",
-        "Add a Role",
-        "Add an Employee",
-        new inquirer.Separator(),
-        "View all Department",
-        "View all Role",
-        "View all Employee",
-        new inquirer.Separator(),
-        "Update a Department",
-        "Update a Role",
-        "Update an Employee"
-      ],
-      loop: false
-    });
-    switch (todoAnswer.todo) {
-      case "Add a Department":
-        const { department } = await inquirer.prompt({
-          type: "input",
-          name: "department",
-          message: "What is the name of the department?"
-        });
-        await addDepartment(department);
-        return console.log("Added a new department", department);
-      case "Add a Role":
-        const { title, salary, departmentId } = await inquirer.prompt([
-          {
+  async function todo() {
+    try {
+      const todoAnswer = await inquirer.prompt({
+        type: "list",
+        name: "todo",
+        message: "What do you want to do?",
+        choices: [
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          new inquirer.Separator(),
+          "View all Departments",
+          "View all Roles",
+          "View all Employees",
+          new inquirer.Separator(),
+          "Update a Department",
+          "Update a Role",
+          "Update an Employee"
+        ],
+        loop: false
+      });
+      switch (todoAnswer.todo) {
+        case "Add a Department":
+          const { department } = await inquirer.prompt({
             type: "input",
-            name: "title",
-            message: "What is the title of the role?"
-          },
-          {
-            type: "input",
-            name: "salary",
-            message: "What is the salary for the role?",
-            validate: salary =>
-              !isNaN(salary) ? true : "Please provide a valid salary(number)"
-          },
-          {
-            type: "list",
-            name: "departmentId",
-            message: "select a department",
-            choices: async () => {
-              const departments = await getDepartments();
-              return departments.map(d => ({ name: d.name, value: d.id }));
+            name: "department",
+            message: "What is the name of the department?"
+          });
+          addDepartment(department).then(() => {
+            todo();
+            return console.log("Added a new department", department);
+          });
+        case "View all Departments":
+          const allDepartments = await getDepartments();
+          console.table(allDepartments);
+          return todo();
+        case "Add a Role":
+          const { title, salary, departmentId } = await inquirer.prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What is the title of the role?"
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "What is the salary for the role?",
+              validate: salary =>
+                !isNaN(salary) ? true : "Please provide a valid salary(number)"
+            },
+            {
+              type: "list",
+              name: "departmentId",
+              message: "select a department",
+              choices: async () => {
+                const departments = await getDepartments();
+                return departments.map(d => ({ name: d.name, value: d.id }));
+              }
             }
-          }
-        ]);
-        await addRole({ title, salary, departmentId });
-        return console.log("Role added!");
-      case "Add an Employee":
-        const {
-          firstName,
-          lastName,
-          roleId,
-          managerId
-        } = await inquirer.prompt([
-          {
-            type: "input",
-            name: "firstName",
-            message: "What is the Employee's first name?"
-          },
-          {
-            type: "input",
-            name: "lastName",
-            message: "What is the Employee's last name?"
-          },
-          {
-            type: "list",
-            name: "roleId",
-            message: "select a Role",
-            choices: async () => {
-              const roles = await getRoles();
-              return roles.map(d => ({ name: d.title, value: d.id }));
+          ]);
+          await addRole({ title, salary, departmentId });
+          return console.log("Role added!");
+        case "View all Roles":
+          const allRoles = await getRoles();
+          console.table(allRoles);
+          return todo();
+        case "Add an Employee":
+          const {
+            firstName,
+            lastName,
+            roleId,
+            managerId
+          } = await inquirer.prompt([
+            {
+              type: "input",
+              name: "firstName",
+              message: "What is the Employee's first name?"
+            },
+            {
+              type: "input",
+              name: "lastName",
+              message: "What is the Employee's last name?"
+            },
+            {
+              type: "list",
+              name: "roleId",
+              message: "select a Role",
+              choices: async () => {
+                const roles = await getRoles();
+                return roles.map(d => ({ name: d.title, value: d.id }));
+              }
+            },
+            {
+              type: "list",
+              name: "managerId",
+              message: "select a Manager",
+              choices: async () => {
+                const employees = await getEmployees();
+                return employees.map(d => ({
+                  name: `${d.first_name} ${d.last_name}`,
+                  value: d.id
+                }));
+              }
             }
-          },
-          {
-            type: "list",
-            name: "managerId",
-            message: "select a Manager",
-            choices: async () => {
-              const employees = await getEmployees();
-              return employees.map(d => ({
-                name: `${d.first_name} ${d.last_name}`,
-                value: d.id
-              }));
-            }
-          }
-        ]);
-        await addEmployee({ firstName, lastName, roleId, managerId });
-        return console.log("Employee added!");
+          ]);
+          await addEmployee({ firstName, lastName, roleId, managerId });
+          return console.log("Employee added!");
+        case "View all Employees":
+          const allEmployees = await getEmployees();
+          return console.table(allEmployees);
+      }
+    } catch (err) {
+      console.log("an error occuredd==>>>", err);
     }
-  } catch (err) {
-    console.log("an error occuredd==>>>", err);
   }
+  todo();
 });
--- schema.sql
+++ schema.sql
@@ -1,29 +1,48 @@
-CREATE Database managementSystem; 
+CREATE Database managementSystem;
 
 USE managementSystem;
 
-CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT NOT NULL, 
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department
+(
+  id INTEGER
+  AUTO_INCREMENT NOT NULL, 
+  name VARCHAR
+  (30) NOT NULL,
+  PRIMARY KEY
+  (id)
 );
 
-CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT NOT NULL, 
-  title VARCHAR(30) NOT NULL,
+  CREATE TABLE role
+  (
+    id INTEGER
+    AUTO_INCREMENT NOT NULL, 
+  title VARCHAR
+    (30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY
+    (department_id) REFERENCES department
+    (id),
+  PRIMARY KEY
+    (id)
 );
 
-CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT NOT NULL, 
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+    CREATE TABLE employee
+    (
+      id INTEGER
+      AUTO_INCREMENT NOT NULL, 
+  first_name VARCHAR
+      (30) NOT NULL,
+  last_name VARCHAR
+      (30) NOT NULL,
   role_id INTEGER NOT NULL,
   manager_id INTEGER NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES role(id),FOREIGN KEY (manager_id) REFERENCES employee(id),
-  PRIMARY KEY (id)
+  FOREIGN KEY
+      (role_id) REFERENCES role
+      (id),FOREIGN KEY
+      (manager_id) REFERENCES employee
+      (id),
+  PRIMARY KEY
+      (id)
 );
 
